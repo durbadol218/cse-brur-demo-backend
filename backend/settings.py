7a458dd9d539b62a44914730936a394071a50a96
Durbadol Goswami <goswamidurbadol@gmail.com>
2026-03-22 10:03:58 +0600

    fix:settings.py for cloudinary storage for vercel

--- backend/settings.py
+++ backend/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "unsafe-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = False
+# DEBUG = False
 
 ALLOWED_HOSTS = ["*",".vercel.app",".now.sh","127.0.0.1"]
 
